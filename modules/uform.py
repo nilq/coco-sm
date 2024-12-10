87b0d0c913c60bc7311752cdd7299668ea6ec956
Niels Horn <niels.horn@ogment.ai>
2024-12-10 13:50:32 +0100

    Update UForm module, add environment variable configuration of model and embedding dimensions.

--- modules/uform.py
+++ modules/uform.py
@@ -1,19 +1,30 @@
+import os
 import uform
+import torch
 
-def image_forward_fn(model, images, device, transform):
-    images = model.preprocess_image(images).to(device)
-    return model.encode_image(images)
-
-def text_forward_fn(model, texts, device, transform):
-    texts = model.preprocess_text(texts)
-    texts = {k: v.to(device) for k, v in texts.items()}
-    return model.encode_text(texts)
-
-embedding_dim = 256
+embedding_dim = int(os.getenv("EMBEDDING_DIM", 256))
 image_preprocess = None
 text_preprocess = None
 
-model = uform.get_model(
-    'unum-cloud/uform-vl-multilingual-v2'
+processors, models = uform.get_model(
+    os.getenv("UFORM_MODEL", "unum-cloud/uform3-image-text-multilingual-base")
 )
 
+model_text = models[uform.Modality.TEXT_ENCODER]
+model_image = models[uform.Modality.IMAGE_ENCODER]
+processor_text = processors[uform.Modality.TEXT_ENCODER]
+processor_image = processors[uform.Modality.IMAGE_ENCODER]
+
+def image_forward_fn(model, images, device, transform):
+    model = model_image
+    images = processor_image(images)#.to(device)
+    return model.encode(images)
+
+def text_forward_fn(model, texts, device, transform):
+    model = model_text
+    texts = processor_text(texts)
+    # texts = {k: v.to(device) for k, v in texts.items()}
+    return model.encode(texts)
+
+# NOTE: Dummy model used to satisfy the interface. Not applicable for the now disjoint modalities.
+model = torch.nn.Module()
